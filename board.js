b0e35641a6b8ee9a6eaaefc84799a6430410a3b9
sdorado2 <sdorado2@gmail.com>
2023-04-25 09:40:20 -0400

    moving components to their own files

---
+++ board.js
@@ -0,0 +1,12 @@
+class createBoard {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+const boardOne = new createBoard(610, 525);
+
+const boardTwo = new createBoard(610, 525);
+
+export { boardOne, boardTwo };
