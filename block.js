817a1a5cd98199d9b59b7310a49af8c1c39c7c4d
sdorado2 <sdorado2@gmail.com>
2023-04-25 11:21:58 -0400

    moving and updating block functions to better reflect the action taking place

--- block.js
+++ block.js
@@ -1,8 +1,24 @@
 class makeBlock {
-  constructor(x_axis, y_axis, width, height) {
+  constructor(x_axis) {
     this.x_axis = x_axis;
-    this.y_axis = y_axis;
-    this.width = width;
-    this.height = height;
+    this.y_axis = 300;
+    this.width = 180;
+    this.height = 80;
   }
+}
+
+function playerOneBlock() {
+  return { x_axis: Math.floor(Math.random() * 4 + 3) * 100 + 50 };
+}
+
+function playerOneBlock() {
+  return { x_axis: Math.floor(Math.random() * 4 + 6) * 100 + 1050 };
+}
+
+function createBlock() {
+  let newBlock;
+  while (newBlock === null) {
+    newBlock = playerOneBlock;
+  }
+  return newBlock;
 }
