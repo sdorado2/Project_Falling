b57703bc33cfd68e598fd526ee6b843a9cd82481
sdorado2 <sdorado2@gmail.com>
2024-09-02 17:15:47 -0400

    updated code for board creation

--- board.js
+++ board.js
@@ -1,19 +1,30 @@
-const templateBoard = { width: 610, height: 525 };
+const templateBoard = { width: 38.125, height: 32.813 };
 
-const displayBoardOne = document.querySelector(".gridBlock");
-const styleBoardOne = `
-    width : ${templateBoard.width}px;
-    height : ${templateBoard.height}px;
-    background-color : tomato; 
-`;
-displayBoardOne.style.cssText = styleBoardOne;
+const displayBoardOne = buildBoard("tomato", "gridBlock");
+const displayBoardTwo = buildBoard("steelblue", "gridBlockC2");
 
-const displayBoardTwo = document.querySelector(".gridBlockC2");
-const styleBoardTwo = `
-    width : ${templateBoard.width}px;
-    height : ${templateBoard.height}px;
-    background-color : steelblue;
-`;
-displayBoardTwo.style.cssText = styleBoardTwo;
+function buildBoard(color, parentName) {
+	const parent = document.querySelector(`.${parentName}`);
+	const addStyle = ` width : ${templateBoard.width}em;
+	height : ${templateBoard.height}; background-color: ${color}`;
+	parent.style.cssText = addStyle;
+	return parent;
+}
+
+// const displayBoardOne = document.querySelector(".gridBlock");
+// const styleBoardOne = `
+//     width : ${templateBoard.width}em;
+//     height : ${templateBoard.height}em;
+//     background-color : tomato;
+// `;
+// displayBoardOne.style.cssText = styleBoardOne;
+
+// const displayBoardTwo = document.querySelector(".gridBlockC2");
+// const styleBoardTwo = `
+//     width : ${templateBoard.width}em;
+//     height : ${templateBoard.height}em;
+//     background-color : steelblue;
+// `;
+// displayBoardTwo.style.cssText = styleBoardTwo;
 
 export { displayBoardOne, displayBoardTwo };
